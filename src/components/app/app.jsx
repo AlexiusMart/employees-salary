af8fda331e6adb2494b1a509e446c4816225d509
iStre1ok <am-nf@yandex.ru>
2023-01-18 14:50:28 +0300

    complete search panel

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -16,7 +16,8 @@ class App extends Component {
                 {name: 'Michel S.', salary: 3000, premium: false, like: false, id: 1},
                 {name: 'Alex M.', salary: 800, premium: true, like: true, id: 2},
                 {name: 'Leo K.', salary: 5000, premium: false, like: false, id: 3}
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -54,10 +55,27 @@ class App extends Component {
             })
         }))
     }
+
+    searchEmployees = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+        
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
     
     render() {
+        const {data, term} = this.state
         const sumEmp = this.state.data.length;
         const sumPremEmp = this.state.data.filter(item => item.premium).length;
+        const visibleData = this.searchEmployees(data, term);
+
         return (
             <div className="app">
                 <AppInfo
@@ -65,12 +83,12 @@ class App extends Component {
                     sumPremEmp={sumPremEmp} />
     
                 <div className="search-panel">
-                    <SearchPanel />
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch} />
                     <AppFilter />
                 </div>
     
                 <EmployeesList
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp} />
     
